f0ec2379322ca76b08fc33efb558d2b3f57ac928
UmzilaEcommerce <umzilaecommerce@gmail.com>
2025-12-25 20:10:45 +0200

    Remove hardcoded Supabase keys and load client config securely

---
+++ netlify/functions/get-client-config.js
@@ -0,0 +1,13 @@
+export async function handler() {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify({
+      SUPABASE_URL: process.env.SUPABASE_URL,
+      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY
+    })
+  };
+}
