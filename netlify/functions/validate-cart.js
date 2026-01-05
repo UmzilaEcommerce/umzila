013da62da229ef748e20bfac484f8431df7a7afc
Ntando <umzilaecommerce@gmail.com>
2026-01-05 17:47:13 +0200

    changing the verify cart to check cart correctly

--- netlify/functions/validate-cart.js
+++ netlify/functions/validate-cart.js
@@ -21,7 +21,7 @@ exports.handler = async function(event, context) {
         
         // Initialize Supabase
         const supabaseUrl = process.env.SUPABASE_URL;
-        const supabaseKey = process.env.SUPABASE_SERVICE_KEY;
+        const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
         
         if (!supabaseUrl || !supabaseKey) {
             return {
